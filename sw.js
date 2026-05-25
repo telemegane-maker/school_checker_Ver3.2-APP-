32d74119906c48dc03a08d18fbd2c5dda8e9c765
telemegane-maker <telemegane@gmail.com>
2026-05-25 16:55:40 +0900

    Update service worker cache version and improve code

--- sw.js
+++ sw.js
@@ -1,24 +1,29 @@
-const CACHE_NAME = 'sync-lite-v1';
+const CACHE_NAME = 'sync-lite-v3.4';
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icon-192.png',
-    './icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// インストール時にファイルをキャッシュ
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
-// 圏外でもキャッシュからファイルを返す
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
